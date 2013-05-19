8e1f0aec72f17831497a7f51b92d02e4cfb2ff84
Pavel Ivon <ivon@sssvt.cz>
2013-05-19 11:02:40 +0200

    Initial state
    Created MainActivity containing simple GMap with static markers
    Connected to Google Play Services for GMaps v2

---
+++ src/cz/ghclient/MainActivity.java
@@ -0,0 +1,45 @@
+package cz.ghclient;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.Menu;
+
+public class MainActivity extends FragmentActivity {
+	static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+	static final LatLng KIEL = new LatLng(53.551, 9.993);
+	private GoogleMap map;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+				.getMap();
+
+		if (map != null) {
+			Marker hamburg = map.addMarker(new MarkerOptions()
+					.position(HAMBURG).title("Hamburg"));
+			Marker kiel = map.addMarker(new MarkerOptions()
+					.position(KIEL)
+					.title("Kiel")
+					.snippet("Kiel is cool")
+					.icon(BitmapDescriptorFactory
+							.fromResource(R.drawable.ic_launcher)));
+		}else{
+			Log.e("ghclient", "cannot prepare map");
+		}
+
+	}
+}
